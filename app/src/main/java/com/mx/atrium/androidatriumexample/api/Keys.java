d5fde022ebbf60085bcae0f858c55196d9c82e7b
Robert Montgomery <rob@heizenrader.com>
2017-04-12 00:15:34 -0600

    Adding empty Key file.

---
+++ app/src/main/java/com/mx/atrium/androidatriumexample/api/Keys.java
@@ -0,0 +1,11 @@
+package com.mx.atrium.androidatriumexample.api;
+
+/**
+ * Created by robmontgomery on 10/6/16.
+ */
+
+public class Keys {
+    public static final String MX_API_URL = "https://vestibule.mx.com/";
+    public static final String MX_CLIENT_ID = "CLIENT_ID";
+    public static final String MX_API_KEY = "KEY";
+}
